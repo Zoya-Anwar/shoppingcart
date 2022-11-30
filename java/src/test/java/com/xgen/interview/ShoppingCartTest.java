4a6d0eafecf2f5272315938855117c8db4dfcd0f
zoyaanwar@hotmail.com <zoyaanwar@hotmail.com>
2022-11-30 12:20:46 +0000

    Add tests for new features

--- java/src/test/java/com/xgen/interview/ShoppingCartTest.java
+++ java/src/test/java/com/xgen/interview/ShoppingCartTest.java
@@ -17,7 +17,7 @@ public class ShoppingCartTest {
 	
 	@Before
 	public void beforeTests() {
-		sc = new ShoppingCart(new Pricer());
+		sc = new ShoppingCart(pricer);
 	}
 	
     @Test
@@ -53,6 +53,48 @@ public class ShoppingCartTest {
         assertTrue(sc.getContents().containsKey("crisps"));
         assertEquals(2, sc.getItemCount());
     }
+    
+	@Test 
+	public void emptyShoppingCart() {
+        assertEquals(0, sc.getItemCount());
+	}
+	
+    @Test
+    public void canAddMultipleSameItems() { 
+        sc.addItem("apple", 2);
+        sc.addItem("apple", 2);
+        assertTrue(sc.getContents().containsKey("apple"));
+        assertEquals(4, sc.getItemCount());
+    }
+    
+    @Test 
+    public void canFormatPriceLastReceiptInOrderGiven() {
+        sc.addItem("apple", 2);
+        sc.addItem("banana", 1);
+        sc.addItem("apple", 1);
+        
+        final ByteArrayOutputStream myOut = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(myOut));
+        sc.printReceipt();
+        String result = myOut.toString();
+        
+        assertEquals(String.format("apple - 3 - €3.00%nbanana - 1 - €2.00%nTotal - €5.00%n"), result);
+    }
+    
+    @Test 
+    public void canFormatPriceFirstReceipt() {
+        sc.addItem("apple", 2);
+        sc.addItem("banana", 1);
+        sc.addItem("apple", 1);
+        
+        final ByteArrayOutputStream myOut = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(myOut));
+        sc.printReceipt();
+        String result = myOut.toString();
+        
+        assertEquals(String.format("€3.00 - apple - 3%n€2.00 - banana - 1%n€5.00 - Total%n"), result);
+    }
+    
 }
 
 
